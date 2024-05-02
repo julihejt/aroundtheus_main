6d284178fb8e439835757330abd6a90ab67087a2
Julie H <153778807+julihejt@users.noreply.github.com>
2024-05-02 10:35:18 +0200

    Add files via upload

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,20 @@
+// PopupWithImage.js
+
+import Popup from "./Popup.js";
+
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super({ popupSelector });
+    this._imageElement = this._popupElement.querySelector(
+      ".modal__preview-image"
+    );
+    this._imageTitle = this._popupElement.querySelector(".modal__preview-name");
+  }
+
+  open(data) {
+    this._imageElement.src = data.link;
+    this._imageElement.alt = data.name;
+    this._imageTitle.textContent = data.name;
+    super.open();
+  }
+}
